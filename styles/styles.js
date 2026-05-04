f35b7d58ae826752740ceabf08ca4232b3ad51c6
givanz <givanz@gmail.com>
2026-05-04 19:54:53 +0300

    Navbar menu centered

--- styles/styles.js
+++ styles/styles.js
@@ -1,12 +1,27 @@
-Vvveb.Styles.add("base/gold-dark", {
+Vvveb.Styles.add("base/an", {
+    name: "An",
+    image: Vvveb.themeBaseUrl + "/screenshots/styles/base/an-thumb.webp",
+    html: "",
+    style: `css/styles/base/an.css`
+});Vvveb.Styles.add("base/gold-dark", {
     name: "Gold dark",
     image: Vvveb.themeBaseUrl + "/screenshots/styles/base/gold-dark-thumb.webp",
     html: "",
     style: `css/styles/base/gold-dark.css`
+});Vvveb.Styles.add("base/ideea", {
+    name: "Ideea",
+    image: Vvveb.themeBaseUrl + "/screenshots/styles/base/ideea-thumb.webp",
+    html: "",
+    style: `css/styles/base/ideea.css`
 });Vvveb.Styles.add("base/marcellus", {
     name: "Marcellus",
     image: Vvveb.themeBaseUrl + "/screenshots/styles/base/marcellus-thumb.webp",
     html: "",
     style: `css/styles/base/marcellus.css`
+});Vvveb.Styles.add("base/visionary", {
+    name: "Visionary",
+    image: Vvveb.themeBaseUrl + "/screenshots/styles/base/visionary-thumb.webp",
+    html: "",
+    style: `css/styles/base/visionary.css`
 });
-	Vvveb.StylesGroup['Base'] = ["base/gold-dark","base/marcellus"];
+	Vvveb.StylesGroup['Base'] = ["base/an","base/gold-dark","base/ideea","base/marcellus","base/visionary"];
